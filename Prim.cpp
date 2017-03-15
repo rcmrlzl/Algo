30242a33f467cfa74d5c36283b0f6b3a2e3d75de
rcmrlzl <243588929@qq.com>
2017-03-15 19:41:09 +0800

    Add files via upload

---
+++ Prim.cpp
@@ -0,0 +1,56 @@
+﻿#include<iostream>
+#include<algorithm>
+using namespace std;
+#define MAX 100
+#define MAXCOST 100000000
+int Prime(int graph[][MAX],int n){
+    int lowcost[MAX];
+    int mst[MAX];
+    int i,j, min,minid,sum = 0;
+    for(i=2;i<=n;i++){
+        lowcost[i] = graph[1][i];
+        mst[i] = 1;
+    }
+    mst[1] = 0;
+    for(i=2;i<=n;i++){
+        min = MAXCOST;
+        minid = 1;
+        for(j=2;j<=n;j++){
+            if(lowcost[j]<min&&lowcost[j]!=0){
+                min = lowcost[j];
+                minid = j;
+            }
+        }
+        cout<<"V"<<mst[minid]<<"-"<<"V"<<minid<<":"<<min<<endl;
+        lowcost[minid] = 0;
+        sum+=min;
+        for(j=2;j<=n;j++){
+            if(graph[minid][j]<lowcost[j]){
+                lowcost[j] = graph[minid][j];
+                mst[j] = minid;
+            }
+        }
+    }
+    return sum;
+}
+
+int graph[MAX][MAX];
+int main() {
+    int m,n;
+    int v1,v2,cost;
+    cin>>m>>n;
+    for(int i=1;i<=m;i++){
+        for(int j=1;j<=m;j++){
+            graph[i][j] = MAX;
+            graph[j][i] = MAX;
+        }
+    }
+    for(int i=0;i<n;i++){
+        cin>>v1>>v2>>cost;
+        graph[v1][v2] = cost;
+        graph[v2][v1] = cost;
+    }
+    int sum = Prime(graph, m);
+    cout<<"最小权值为："<<sum<<endl;
+    return 0;
+}
---
+++ dijkstra.cpp
@@ -0,0 +1,65 @@
+﻿#include<iostream>
+#include<algorithm>
+using namespace std;
+#define MAXNUM 100
+#define MAXINT 100000
+int dist[MAXNUM];
+int m,n;
+bool S[MAXNUM];
+void dijkstra(int A[][MAXNUM],int v){
+	int prev[100];
+	for(int i=1;i<=v;i++){
+		dist[i] = A[1][i];
+		S[i] = false;
+		if(dist[i] == MAXINT)
+			prev[i] = -1;
+		else prev[i] = 1;
+	}
+
+	dist[1] = 0;
+	S[1] = true;
+
+	for(int i=2;i<=v;i++){
+		int mindist = MAXINT;
+		int u;
+		for(int j=1;j<=v;j++){
+			if(S[j]==0&&dist[j]<mindist){
+				mindist = dist[j];
+				u = j;
+			}
+		}
+		cout<<"u="<<u<<endl;
+		S[u] = true;
+		for(int j=1;j<=v;j++){
+			if(S[j]==0&&A[u][j]<MAXINT){
+				if(dist[u]+A[u][j]<dist[j]){
+					dist[j] = dist[u]+A[u][j];
+					prev[j] = u;
+				}
+			}
+		}
+	}
+}
+
+int main(){
+
+	int v1,v2,cost;
+	int a[MAXNUM][MAXNUM];
+	for(int i=0;i<MAXNUM;i++){
+		for(int j=0;j<MAXNUM;j++){
+			a[i][j] = MAXINT;
+		}
+	}
+	cin>>m>>n;
+	for(int i=1;i<=n;i++){
+		cin>>v1>>v2>>cost;
+		a[v1][v2] = cost;
+		a[v2][v1] = cost;
+	}
+	dijkstra(a,m);
+	for(int i=1;i<=m;i++){
+		cout<<dist[i]<<endl;
+	}
+	cout<<dist[5];
+	return 0;
+}
