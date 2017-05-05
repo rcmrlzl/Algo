d1546ab4762ba45b54de402c4f5053746d52db13
rcmrlzl <243588929@qq.com>
2017-05-05 15:10:11 +0800

    Update maxSubArray.cpp

--- maxSubArray.cpp
+++ maxSubArray.cpp
@@ -1,14 +1,13 @@
 #include<iostream>
 using namespace std;
-int maxSubArray(int a[],int nums) {
-        int sum = 0, minSum = 0, maxSum = INT_MIN;
-        for (int i = 0; i < nums; i++) {
-            sum += a[i];
-            maxSum = max(maxSum, sum - minSum);
-            minSum = min(minSum, sum);
-        }
-        return maxSum;
-    }
+int maxSubArray(vector<int> nums){
+	int res = INT_MIN,sum = 0;
+	for(int num:nums){
+		sum = max(sum+num,num);
+		res = max(res,sum);
+	}
+	return res;
+}
 
 int main(){
 	int a[9] = {-2,1,-3,4,-1,2,1,-5,4};
